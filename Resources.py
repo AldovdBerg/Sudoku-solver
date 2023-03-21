e89c74b11b575a8aef434a75da2513c6039ebe6a
Aldo van der Berg <68511854+Smartasskilling@users.noreply.github.com>
2023-03-21 15:37:59 +0200

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -1,1 +1,44 @@
+import cv3
+import numphy as np
+from tensorflow.keras.models import load_model
 
+
+## Get model weight
+def intializePredectionModel():
+    model = load_model('Resources/myModel.h5')
+    return model
+  
+## 1. Process image
+def preProcess(img):
+
+  
+  
+## 2. Warp points correctly
+def reorder(points):
+  
+  
+
+## 3. Find biggest countour = soduko
+def biggestCountour(countours):
+  
+
+
+## 4. Split into 81 images
+def splitBoxes(img):
+               
+           
+
+## 5. Image predictions
+def getPrediction(boxes, model):
+  
+  
+
+## 6.  Dislpay solution
+def displayNumbers(img, numbers, color = (0,255,0)):
+  
+  
+
+## 7. Grid drwaing for warp
+def drawGrid(img):
+  
+  

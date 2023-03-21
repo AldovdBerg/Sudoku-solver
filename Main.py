93b7eb5dc5a6341a0b68b53e36d3daf731e65960
Aldo van der Berg <68511854+Smartasskilling@users.noreply.github.com>
2023-03-21 15:24:22 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,3 +2,35 @@ import os
 from Resources import *
 import Solver
 import argparse
+
+####
+imgHeight = 450
+imgWidth = 450
+ap = argparse.ArgumentParser()
+ap.add.argument("-i", "--image", default = 'recorces/1.jpg', help = "Path to input image")
+args = vars(ap.parse_args())
+model = initializePredectionModel()  #for cnn mode
+####
+
+
+
+## 1. Image prep
+
+
+
+## 2. Find contours
+
+
+## 3. Biggest contour = soduko
+
+
+  ## 4. Find digits
+  
+  
+  ## 5. Solve
+  
+  
+  ## 6. Display solution
+
+
+

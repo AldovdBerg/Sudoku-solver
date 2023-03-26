5495a92d12ceca49c1fbd4435d2ebdea97225cab
Aldo van der Berg <68511854+Smartasskilling@users.noreply.github.com>
2023-03-26 16:16:21 +0200

    Update Resources.py
    Added roles

--- Resources.py
+++ Resources.py
@@ -8,37 +8,37 @@ def intializePredectionModel():
     model = load_model('Resources/myModel.h5')
     return model
   
-## 1. Process image
+## 1. Process image,, @Tarryn Collins
 def preProcess(img):
 
   
   
-## 2. Warp points correctly
+## 2. Warp points correctly, #Christoph Snell
 def reorder(points):
   
   
 
-## 3. Find biggest countour = soduko
+## 3. Find biggest countour = soduko, @Christoph Snell
 def biggestCountour(countours):
   
 
 
-## 4. Split into 81 images
+## 4. Split into 81 images, @Obelya Fourie
 def splitBoxes(img):
                
            
 
-## 5. Image predictions
+## 5. Image predictions, @Johan Michael Lourens
 def getPrediction(boxes, model):
   
   
 
-## 6.  Dislpay solution
+## 6.  Dislpay solution, @Obelya Fourie
 def displayNumbers(img, numbers, color = (0,255,0)):
   
   
 
-## 7. Grid drwaing for warp
+## 7. Grid drwaing for warp, @Randall Traz Mocke
 def drawGrid(img):
   
   

7f6b8ab3cddbab1e1f413bca1332d5a130510c46
TarrynCollins4 <113286375+TarrynCollins4@users.noreply.github.com>
2023-03-28 19:08:00 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,11 +15,21 @@ model = initializePredectionModel()  #for cnn mode
 
 
 ## 1. Image prep, @Tarryn Collins
-
+image = cv2.imread(args["image"])
+image = cv2.resize(image, (imgWidth, imgHeight))
+#Resizing image to a square image
+blankImg = np.zeros((imgHeight, imgWidth,3),np.uint8)
+#Creating a blank image for testing
+tresholdImg = preProcess(image)
 
 
 ## 2. Find contours, @Tarryn Collins
-
+imageContours = image.copy()
+#Copying image for display
+imageBigContour = image.copy()
+contours, hierarchy = cv2.findContours(tresholdImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#Find contours
+cv2.contourDrawing(imageContours, contours, -1, (0, 255, 0),3)
 
 
 ## 3. Biggest contour = soduko, @Christoph Snell

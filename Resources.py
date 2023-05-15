00a17a014830243852df1e87412d48053bd039e1
Aldo van der Berg <68511854+Smartasskilling@users.noreply.github.com>
2023-05-15 15:27:44 +0200

    Add files via upload

--- Resources.py
+++ Resources.py
@@ -1,5 +1,5 @@
 import cv2
-import numphy as np
+import numpy as np
 from tensorflow.keras.models import load_model
 
 
@@ -7,7 +7,7 @@ from tensorflow.keras.models import load_model
 def intializePredectionModel():
     model = load_model('Resources/myModel.h5')
     return model
-  
+
 ## 1. Process image,, @Tarryn Collins
 def preProcess(img):
     grayImage = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -18,36 +18,36 @@ def preProcess(img):
     #Applying adaptive treshold
     return tresholdImg
 
-  
-  
+
+
 ## 2. Warp points correctly, #Christoph Snell
 def reorder(points):
     points = points.reshape((4, 2))
     pointsNew = np.zeros((4, 1, 2), dtype=np.int32)
     add = points.sum(1)
     pointsNew[0] = points[np.argmin(add)]
-    pointsNew[3] =points[np.argmax(add)]
+    pointsNew[3] = points[np.argmax(add)]
     diff = np.diff(points, axis=1)
-    pointsNew[1] =points[np.argmin(diff)]
+    pointsNew[1] = points[np.argmin(diff)]
     pointsNew[2] = points[np.argmax(diff)]
     return pointsNew
-  
-  
+
+
 
 ## 3. Find biggest countour = soduko, @Christoph Snell
 def biggestContour(countours):
-        biggest = np.array([])
+    biggest = np.array([])
     max_area = 0
-    for r in contours:
-        area = cv2.contourArea(i)
+    for r in countours:
+        area = cv2.contourArea(r)
         if area > 50:
             peri = cv2.arcLength(r, True)
-            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            approx = cv2.approxPolyDP(r, 0.02 * peri, True)
             if area > max_area and len(approx) == 4:
                 biggest = approx
                 max_area = area
     return biggest,max_area
-  
+
 
 
 ## 4. Split into 81 images, @Obelya Fourie
@@ -63,7 +63,7 @@ def splitBoxes(img):
 
 ## 5. Image predictions, @Johan Michael Lourens
 def getPrediction(boxes, model):
-   result = []
+    result = []
     for image in boxes:
         ## PREPARE IMAGE
         img = np.asarray(image)
@@ -81,19 +81,21 @@ def getPrediction(boxes, model):
         else:
             result.append(0)
     return result
-  
+
 
 ## 6.  Dislpay solution, @Obelya Fourie
 def displayNumbers(img,nums,color = (0,255,0)):
     secW = int(img.shape[1]/9)
     secH = int(img.shape[0]/9)
+    print(nums)
     for m in range (0,9):
         for n in range (0,9):
             if nums[(n*9)+m] != 0 :
-                 cv2.putText(img, str(nums[(n*9)+m]),
-                               (m*secW+int(secW/2)-10, int((n+0.8)*secH)), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, color, 2, cv2.LINE_AA)
+                cv2.putText(img, str(nums[(n*9)+m]),
+                            (m*secW+int(secW/2)-10, int((n+0.8)*secH)),
+                            cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, color, 2, cv2.LINE_AA)
     return img
-  
+
 
 ## 7. Grid drwaing for warp, @Randall Traz Mocke
 #must decode abit further
